6055a5e2aa9caa0e085fd97a79976764edeb5306
Ritu0097 <rituchaudhary0097@gmail.com>
2024-04-07 13:38:32 +0530

    render

--- src/components/App.js
+++ src/components/App.js
@@ -2,10 +2,21 @@ import React, {Component, useState} from "react";
 import '../styles/App.css';
 
 const App = () => {
-  return (
-    <div id="main"></div>
-  )
-}
+  const [projects, setProjects] = useState([
+    { name: "Project 1", description: "Description of Project 1" },
+    { name: "Project 2", description: "Description of Project 2" }
+  ]);
 
+  return (
+    <div id="main">
+      {projects.map((project, index) => (
+        <div key={index}>
+          <h1 data-ns-test="project-name">{project.name}</h1>
+          <h6 data-ns-test="project-description">{project.description}</h6>
+        </div>
+      ))}
+    </div>
+  );
+};
 
 export default App;
